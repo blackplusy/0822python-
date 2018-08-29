2f1eb5dac14b0233bbd504a8784812011e45a29f
blackplusy <happyming07@sina.com>
2018-08-29 14:42:13 +0800

    Add files via upload

---
+++ 例子-0829-03.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+
+print(10+3)    	#13
+print(10-3)	#7	
+print(10*3)	#30
+print(10/3)	#3.333333
+print(10%3)	#1
+print(10**3)	#1000
+print(10//3)	#3
---
+++ 例子-0829-04.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+#假 and 真 ==假
+print(1>2 and 2<3)
+
+#真 or 假 ==真
+print(1>0 or 2<0)
+
+#非假==真
+print(not(1<0))
