e2f08bd89dcaa8c4b74e2dc7a891043cc36d7014
blackplusy <happyming07@sina.com>
2018-08-29 15:22:28 +0800

    Add files via upload

---
+++ 例子-0829-05.py
@@ -0,0 +1,5 @@
+#coding=utf-8
+print(1==1)    	#True 
+print(2>=2)	#True
+print('a'>'A')  #True
+print('张'>'王')#假
---
+++ 例子-0829-06.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+
+name='hello world!!'
+for i in name:
+	print(i)
+
+li=['heygor','6god']
+for a in li:
+	print(a)
+
+tup=(1,2,3)
+for i in tup:
+	print(i)
+
+if   'e'  in name:
+	print('im in here!!')
