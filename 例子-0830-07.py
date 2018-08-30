4c24353f4f2390ae78b489dece3e289c53c34305
blackplusy <happyming07@sina.com>
2018-08-30 15:15:20 +0800

    Add files via upload

---
+++ 例子-0830-07.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+
+print('python')
+
+print("python")
+
+print('''python''')
+
+#print('i'm name 'heygor'') #有问题
+
+print("i'm name heygor")
+
+
+print('i"m name heygor')
+
+print('''
+	name is hyeygor
+	age  is 18
+''')
